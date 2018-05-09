e6639375ad53cf7337fc056f63dfbc08d4053298
Gabor <gc1569@nyu.edu>
2018-05-09 11:01:50 +0400

    uplaod files

---
+++ bilstm-word2vec/double_log.py
@@ -0,0 +1,24 @@
+from __future__ import print_function
+import logging
+import __builtin__ 
+
+open('cnn.log', 'w').close()
+try:
+    logger
+except NameError:
+    logger = logging.getLogger('cnn.log')
+    logger.setLevel(logging.DEBUG)
+    fh = logging.FileHandler('cnn.log')
+    formatter = logging.Formatter('%(asctime)s - %(message)s')
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)     
+    
+def print(*args, **kwargs):
+    msg = ""
+    for i in args:
+        try:
+            msg = msg + str(i) + ' '
+        except: 
+            continue
+    logger.debug(msg)
+    return __builtin__.print(*args, **kwargs)
